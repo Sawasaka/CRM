5ddead0280b1e56fab19e0dac4702acf71feb3ec
Sawasaka <h.sawasaka@rookiesmart.jp>
2026-07-20 17:16:39 +0900

    fix: use existing consultation calendar

--- apps/web/lib/consultation-calendar.ts
+++ apps/web/lib/consultation-calendar.ts
@@ -1,5 +1,5 @@
 const CONSULTATION_SCHEDULE_ID =
-  'AcZssZ1FVEeSK89Ga-vs1lbkfFsAF-QZwSRDXO2o0siuxw6cHLH550CVjDlr05v7m8DR-5CHAM9rbUUH'
+  'AcZssZ2Q9AOnWDVVzFloRtin2xokAICf8UupW_3t_xXP3c-FoxXpIcCXYJhWu5hwXvvs2ASTgCgndYRE'
 
 export const CONSULTATION_BOOKING_URL =
   `https://calendar.google.com/calendar/appointments/schedules/${CONSULTATION_SCHEDULE_ID}`
